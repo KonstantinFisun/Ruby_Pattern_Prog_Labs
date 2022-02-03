fc63f6667f55198f60f57569383268c2d7668971
Константин Сергеевич Фисун <s0153500@edu.kubsu.ru>
2022-02-03 23:04:42 +0300

    Задание 1.2
    Принять имя пользователя как аргумент программы.
    Поздороваться с пользователем с использованием форматирования
    строки.

--- Laba_1/main.rb
+++ Laba_1/main.rb
@@ -1,1 +1,10 @@
-puts "Hello world, guys!"
+# puts "Hello world, guys!"
+
+def main
+  name = ARGV.first.сapitalize  # Первый аргумент переводим в нужный регистр
+  puts "Приветствую тебя, #{name}"
+end
+
+if __FILE__ == $0
+    main
+end
