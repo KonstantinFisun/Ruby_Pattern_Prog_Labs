9aa506f26e55596509e839f92aaf67885fa37d92
Константин Сергеевич Фисун <s0153500@edu.kubsu.ru>
2022-02-07 11:39:45 +0300

    Задание 3.2
    Написать программу, которая принимает как аргумент два значения. Первое значение говорит, какой из методов задачи 1 выполнить, второй говорит о том, откуда читать список, с клавиатуры, или из файла. Если из файла, то третьим
    аргументом должен быть написан адрес файла. Далее необходимо прочитать массив и выполнить метод.

--- Laba_1/lists.rb
+++ Laba_1/lists.rb
@@ -1,11 +1,36 @@
 def main
+  number = ARGV[0]
+  if(ARGV[1].downcase == "клавиатура")
+    puts("Введите список через пробел и нажмите Enter : ")
+    ARGV.clear
+    list = gets.chomp.split(" ")
+  elsif(ARGV[1].downcase == "файл")
+    file = File.new(ARGV[2], "r")
+    list = file.gets.chomp.split(" ")
+  end
+  list = list.map {|i| i.to_i} # Преобразуем в массив чисел
+  select_metod(number, list)
+end
 
+def select_metod(number, list)
+  case number
+    when "1"
+      puts("Сумма элементов списка - #{sum_el_in_list(list)}")
+    when "2"
+      puts("Произведение элементов списка - #{mul_el_in_list(list)}")
+    when "3"
+      puts("Минимальный элемент списка - #{min_el_in_list(list)}")
+    when "4"
+      puts("Максимальный элемент списка - #{max_el_in_list(list)}")
+    else
+      puts("Некорректный ввод!")
+  end
 end
 
 # Функция нахождения суммы элементов списка
 def sum_el_in_list(list)
   sum = 0
-  for i in list
+  for i in list do
     sum += i
   end
   sum
@@ -36,7 +61,7 @@ def max_el_in_list(list)
   max = list.first
   for i in list
     if(i > max)
-      min = i
+      max = i
     end
   end
   max
