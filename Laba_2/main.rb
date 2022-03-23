ee2059108f5e474bb61102190c850b714fb573e1
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-03-23 15:48:01 +0300

    Задание 1.3
    Создать поле выполнение обязанностей. Реализовать
    методы добавить обязанность, выделить обязанность,
    удалить обязанность, получить текст выделенной
    обязанности, заменить текст выделенной обязанности,
    вывести список обязанностей на экран. Объяснить, почему с точки зрения основных принципов ООП нежелательно получать доступ к массиву целиком.
    Продумать, каким должен быть в этом случае конструктор и внести изменения.

--- Laba_2/main.rb
+++ Laba_2/main.rb
@@ -1,21 +1,73 @@
 class Department
-  attr_accessor :name,:phone
+  attr_accessor :name, :phone
 
-  def initialize (name, phone)
+  def initialize (name, phone, *duty)
   @name, @phone = name, phone
+  @duty = duty
+  @index_duty = -1 # Индекс выбранной обязанности
+  end
+  # Получение информации об объекте
+  def to_s
+    "Имя: #{@name}; Телефон : #{@phone}; \n Обязанности : \n #{duty}"
   end
 
-  def info
-    puts("#{name} : #{phone}")
+  # Вывод всех обязанностей
+  def duty
+    s = ""
+    @duty.each_index{|i| s += "#{i} - #{@duty[i]} \n"}
+    s
   end
+
+  # Добавить обязанность,
+  def duty_add(value)
+    @duty.append(value)
+  end
+
+  # Выделить конкретную обязанность
+  def duty_select(index)
+    @index = index
+  end
+
+  # Удалить обязанность
+  def duty_delete
+    @duty.delete_at(@index)
+    @index = -1
+  end
+
+  # Получить текст выделенной обязанности
+  def get_text_sel_duty
+    @duty[@index]
+  end
+
+  # Заменить текст выделенной обязанности
+  def change_text_sel_duty(value)
+    @duty[@index] = value
+  end
+
 end
 
 def main
-  sales_department = Department.new("Отдел продаж", "+7-923-42-52-522")
-  hr_department = Department.new("Отдел кадров", "+7-425-25-24-235")
+  sales_department = Department.new("Отдел продаж", "+7-923-42-52-522", "Привлечение клиентов")
+  hr_department = Department.new("Отдел кадров", "+7-425-25-24-235", "Расчет премии", "Определение количество рабочих дней")
 
-  sales_department.info
-  hr_department.info
+  # Добавление обязанностей
+  sales_department.duty_add("Работа с целевой аудиторией")
+
+  # Выбрать определенную обязанность
+  sales_department.duty_select(1)
+
+  # Заменить текст выделенной обязанности
+  sales_department.change_text_sel_duty("Бездельничать")
+
+  # Получить текст выделенной обязанности
+  puts(sales_department.get_text_sel_duty)
+
+  # Удаление обязанности
+  sales_department.duty_delete
+
+  # Вывод обязанностей
+  puts(sales_department)
+
 end
 
 if __FILE__ == $0
