897763015d8f3d481a5e5d47dc212a473d969733
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-03-23 13:29:53 +0300

    Задание 1.2
    Создать несколько объектов, вывести информацию о них на экран. Продумать корректный способ вывода информации о текущем состоянии объекта на экран.

--- Laba_2/main.rb
+++ Laba_2/main.rb
@@ -1,12 +1,21 @@
 class Department
   attr_accessor :name,:phone
+
   def initialize (name, phone)
   @name, @phone = name, phone
+  end
+
+  def info
+    puts("#{name} : #{phone}")
   end
 end
 
 def main
+  sales_department = Department.new("Отдел продаж", "+7-923-42-52-522")
+  hr_department = Department.new("Отдел кадров", "+7-425-25-24-235")
 
+  sales_department.info
+  hr_department.info
 end
 
 if __FILE__ == $0
