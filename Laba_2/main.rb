ab92e49a766651ef8c3e5c1804a37b6f743d6544
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-03-29 00:06:25 +0300

    Задание 2.6
    Написать метод write_to_YAML, который записывает массив объектов класса Department в файл YAML.

--- Laba_2/main.rb
+++ Laba_2/main.rb
@@ -1,5 +1,7 @@
 path = File.dirname(__FILE__) # Получили путь к папке
 require "#{path}/Department.rb"
+require "yaml"
+require "yaml/store"
 
 # Считывание всех отделов из файла
 def read_from_txt(file)
@@ -7,6 +9,7 @@ def read_from_txt(file)
   list_departments = [] # Список отделов
   for line in file.readlines
     component = line.chomp.split(';')
+
     list_departments.push(d = Department.new(component[0], component[1]))
     component[2].split(',').each{|x| d.duty_add(x)} # Добавили обязанности
   end
@@ -16,14 +19,21 @@ end
 
 # Запись всех отделов в файл
 def write_to_txt(file, list_departments)
-  file = File.open(file, "w") do |f|
+  File.open(file, "w") do |f|
     list_departments.each{|x| f.puts("#{x.name};#{x.phone};#{x.duty_write_txt}")}
   end
 end
 
 # Вывод всех отделов из списка
-def departments_info(departments)
-  departments.each{|x| puts(x)}
+def departments_info(list_departments)
+  list_departments.each{|x| puts(x)}
+end
+
+# Запись всех отделов в YAML
+def write_to_yaml(file, list_departments)
+  File.open(file,"w") do |f|
+    f.puts YAML.dump(list_departments)
+  end
 end
 
 
@@ -31,9 +41,10 @@ def main
   list_departments = read_from_txt("Department.txt")
   departments_info(list_departments)
 
-  list_departments.push(Department.new("Отдел маркетинга", "+74234252230", "Просмотр истории взаимоотношений"))
+  # list_departments.push(Department.new("Отдел маркетинга", "+74234252230", "Просмотр истории взаимоотношений"))
 
-  write_to_txt("Department.txt", list_departments)
+  # write_to_txt("Department.txt", list_departments)
+  write_to_yaml("Department_write.yaml", list_departments)
 end
 
 if __FILE__ == $0
