d35f563f944fb7067273bf50808656021d357f56
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-04-27 00:26:42 +0300

    Классы данных
    Реализуйте наследника Employee Skilled_employee, добавив два поля о количестве лет опыта и поле строка с описанием опыта работы.

--- Laba_3/Employee.rb
+++ Laba_3/Employee.rb
@@ -16,3 +16,17 @@ class Employee
     @email = email # Электронная почта
   end
 end
+
+class Skilled_employee < Employee
+  # Геттеры и сеттеры
+  attr_accessor :experience, :description_experience
+
+  # Конструктор
+  def initialize(surname:, firstname:, lastname:, bd:, passport:,
+  phone:, address:, email:, experience:, description_experience:)
+  super(surname:surname, firstname:firstname, lastname:lastname, bd:bd,
+    passport:passport, phone:phone, address:address, email:email)
+
+  @experience = experience # Лет опыта
+  @description_experience = description_experience # Описание опыта
+end
