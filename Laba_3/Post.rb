d1e8f93bea2381f424daaeb84fb0e56ff91e8b94
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-04-26 23:23:35 +0300

    Ассоциация
    Добавьте в класс Post поле @salary класса Salary. Реализуйте отношение композиции для этих двух классов, то есть в произвольный момент времени доступ к объектам класса Salary или их создание должно быть возможно только внутри методов объектов класса Post. Продумайте метод salary=, который позволит создавать объект соответствующего наследника класса Salary для объекта класса Post. В методе salary= реализуйте один аргумент — хэш, разберитесь, как это реализуется. Реализуйте геттер, который будет возвращать числовое значение заработной платы.

--- Laba_3/Post.rb
+++ Laba_3/Post.rb
@@ -1,13 +1,13 @@
-Dir[File.dirname(__FILE__) + '/Salary/*.rb'].each {|file| require file }
+Dir[File.dirname(__FILE__) + '/Salary/Salary.rb'].each {|file| require file }
 class Post
   # Геттеры и сеттеры
   attr_accessor :department, :name
-  attr_reader :vacancy, :salary
+  attr_reader :vacancy
 
   # Конструктор
-  def initialize(department,name,salary,vacancy)
+  def initialize(department:,name:,salary:, percent: 0, rub: 0, premium: 0, fine: 0,vacancy:)
     @department, @name, self.vacancy = department, name, vacancy.to_i
-    @salary = salary
+    set_salary(salary:salary, percent:percent, rub:rub, premium:premium, fine:fine) # Вызов сеттер зарплаты
   end
 
   # Конструктор, принимающий строку
@@ -16,22 +16,39 @@ class Post
     new(component[0], component[1], component[2], component[3])
   end
 
+  #=============================================================================
+
+  # Сеттер зарплаты
+  # зарплата, процент надбавки, надбавка в рублях, премия в процентах, штраф
+  def set_salary(salary:, percent:, rub:, premium:, fine:)
+    if self.class.check_salary(salary,percent,rub,premium,fine)
+      @salary = Salary.assembling(salary: salary, percent: percent, rub: rub, premium: premium, fine: fine)
+    else raise ArgumentError.new("Некорректная зарплата!")
+    end
+  end
+
+  # Проверка зарплаты
+  def Post.check_salary(salary, percent, rub, premium, fine)
+    if(salary > 0 and percent>=0 and percent<=100 and rub>=0 and premium>=0 and premium<=100 and fine >= 0)
+      true
+    else
+      false
+    end
+  end
+
+  # Геттер зарплаты
+  def salary
+    @salary.get_salary
+  end
+
+  #=============================================================================
+
   # Сеттер вакансии
   def vacancy=(value)
     if self.class.check_vacancy(value)
       @vacancy = value
     else raise ArgumentError.new("Вакантность должности введена неверно!")
     end
-  end
-
-  # Сеттер зарплаты
-  # def salary=(**value)
-  #
-  # end
-
-  # Проверка зарплаты
-  def Post.check_salary(value)
-
   end
 
   # Проверка вакансии
@@ -48,13 +65,18 @@ class Post
     end
   end
 
+  #========================================================================
+
   # Переопределенный метод to_s
   def to_s
-    "Название: #{name}; Оклад: #{salary}; Должность: #{display_vacancy}"
+    "Название: #{name}; Зарплата: #{salary}; Должность: #{display_vacancy}"
   end
 
+  # Запись в txt
   def write_to_txt
     "#{department};#{name};#{salary};#{vacancy}"
   end
+
+  #========================================================================
 
 end
